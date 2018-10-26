97b165e20ea33be41a12965362ac15e937085acf
Marcus Vinicius Nobre Carraschi <mcarraschi@EGSBR.local>
2018-10-26 14:18:11 -0300

    Enemy Implementation v1.5

--- Project - CC7140/Assets/Scripts/PlatformScene/SpawnPlatforms.cs
+++ Project - CC7140/Assets/Scripts/PlatformScene/SpawnPlatforms.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using UnityEngine;
 
+
     /// <summary>
     /// This code will be generate a stage of game
     /// </summary>
@@ -14,13 +15,17 @@ namespace Assets.Scripts.ObjectsScene
     class SpawnPlatforms : MonoBehaviour{
 
         public int maxPlatform = 20;
+
         public GameObject platform;
+        public GameObject DogAndPlatform;
+
         public float horizontalMin = 7.5f;
         public float horizontalMax = 2f;
         public float verticalMin = 6f;
         public float verticalMax = 6;
 
         private Vector3 originPosition;
+
 
         void Start(){
             originPosition = transform.position;
@@ -34,13 +39,25 @@ namespace Assets.Scripts.ObjectsScene
                 //                UnityEngine.Random.Range(verticalMin, verticalMax),10);
                 //Instantiate(platform, randomPosition, Quaternion.identity);
 
-                Vector3 randomPosition = originPosition +
-                    new Vector3(UnityEngine.Random.Range(horizontalMin, horizontalMax),
-                                -1.43f, 10);
-                Instantiate(platform, randomPosition, Quaternion.identity);
+                if( i % 3 == 0  ){
 
-                originPosition = randomPosition;
-              
+                    Vector3 randomPosition = originPosition +
+                    new Vector3(UnityEngine.Random.Range(horizontalMin, horizontalMax),
+                                0, 0);
+                    Instantiate(DogAndPlatform, randomPosition, Quaternion.identity);
+                    originPosition = randomPosition;                
+                }
+                else{
+
+                    Vector3 randomPosition = originPosition +
+                    new Vector3(UnityEngine.Random.Range(horizontalMin, horizontalMax),
+                                0, 0);
+                    Instantiate(platform, randomPosition, Quaternion.identity);
+
+                    originPosition = randomPosition;
+                }
+
+
             }
         }
 
--- Project - CC7140/Assets/Scripts/Player/CameraFollow.cs
+++ Project - CC7140/Assets/Scripts/Player/CameraFollow.cs
@@ -4,22 +4,61 @@ using System.Linq;
 using System.Text;
 
 using UnityEngine;
+using System.Collections;
 
 namespace Assets.Scripts.Player
 {
     class CameraFollow : MonoBehaviour{
 
-        public Transform target;
+        //public Transform target;
 
-        public float smoothSpeed = 0.125f;
-        public Vector3 offset;
+        //public float smoothspeed = 0.125f;
+        //public Vector3 offset;
 
-        private void Awake(){
-            Vector3 desiredPosition = target.position + offset;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            transform.position = smoothedPosition;
+        //private void awake()
+        //{
+        //    Vector3 desiredposition = target.position + offset;
+        //    Vector3 smoothedposition = Vector3.Lerp(transform.position, desiredposition, smoothspeed);
+        //    transform.position = smoothedposition;
 
-            transform.LookAt(target);
+        //    transform.LookAt(target);
+        //}
+
+        //public float dampTime = 0.15f;
+        //private Vector3 velocity = Vector3.zero;
+        //public Transform target;
+        //Camera cameraObject;
+
+        //void Start(){
+        //    cameraObject = GetComponent<Camera>();
+        //}
+
+        //void Update(){
+
+        //    if (target){
+        //        Vector3 point = cameraObject.WorldToViewportPoint(target.position);
+        //        Vector3 delta = target.position - cameraObject.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+        //        Vector3 destination = transform.position + delta;
+        //        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        //    }
+
+        //}
+
+        //Components
+        private Vector2 velocity;
+        public float smoothTimeY;
+        public float smoothTimeX;
+
+        public GameObject player;
+
+        void Start(){
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        void FixedUpdate(){
+            float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+            float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+            transform.position = new Vector3(posX, posY, transform.position.z);
         }
 
     }
