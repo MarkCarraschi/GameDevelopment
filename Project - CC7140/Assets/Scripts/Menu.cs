039cefcfecfcecd59cd9e2708945fb3aa6e400a5
Marcus Vinicius Nobre Carraschi <mcarraschi@EGSBR.local>
2018-10-30 14:21:11 -0300

    Construct Phase

---
+++ Project - CC7140/Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    InputField name;
+
+    public void PlayGame(){
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame(){
+        Debug.Log("Finalizou");
+        Application.Quit();
+    }
+    
+    public void PlayerLoad()
+    {
+        name = GetComponent<InputField>();
+        Ranking.nomeNPlayer = name.text;
+    }
+
+
+}
